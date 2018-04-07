4ba787dc6a73513f2f96da31f415ca67a6bc211f
yourbestnightmare <prezes.m.sikorski@gmail.com>
2018-04-07 11:35:32 +0200

    Start

---
+++ New Multi Prototype/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float angularSpeed;
+
+	private Rigidbody2D rb;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate(){
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		float speedMultiplier = speed + (y * speed); 
+		rb.velocity = -transform.up * speedMultiplier;
+
+		rb.angularVelocity = -x * angularSpeed + speedMultiplier * 30 * -x;
+	}
+
+}
